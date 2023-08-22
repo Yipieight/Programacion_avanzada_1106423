eee195127823c2c58a757723235969618d62a4f3
Jose Garcia <57154305+Yipieight@users.noreply.github.com>
2023-08-21 18:13:46 -0600

    Lab 2

---
+++ Sabados/Lab 2/Lab2_Jose_Garcia_1106423/Lab2_Jose_Garcia_1106423/Lab2_Jose_Garcia_1106423.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+struct poligonos
+{
+	int largo;
+
+	double Area(int largo, int lados)
+	{
+		double area;
+		double apotema = largo / (2 * tan(3.14 / lados));
+		area = 0.5 * lados * largo * apotema;
+		return area;
+	}
+	double perimetro(int lados, int largo)
+	{
+		double perimetro = lados * largo;
+		return perimetro;
+	}
+};
+
+using namespace::std;
+
+int main()
+{
+	poligonos figura;
+	int numero;
+
+	cout << "¿Que figura desea sacar perímetro y área? \n" << endl;
+	cout << "1. Triangulo (3 lados)" << endl;
+	cout << "2. Cuadricula (4 lados)" << endl;
+	cout << "3. Pentagono (5 lados)" << endl;
+	cout << "4. Hexagono (6 lados)" << endl;
+	cout << "5. Heptagono (7 lados)" << endl;
+	cout << "6. Octagono (8 lados)" << endl;
+	cout << "7. Eneagono (9 lados)" << endl;
+	cout << "8. Decagono (10 lados)" << endl;
+
+	cin >> numero;
+	if (numero >= 10 || numero <= 1)
+	{
+		int lado = numero + 2;
+		cout << "¿Cuando medira el lado?" << endl;
+		cin >> figura.largo;
+		cout << "El área es: " << figura.Area(figura.largo, lado) << "\n";
+		cout << "El perimetro es: " << figura.perimetro(figura.largo, lado);
+	}
+	else
+	{
+		cout << "Este número no esta dentro del rango de la lista";
+	}
+	
+}
